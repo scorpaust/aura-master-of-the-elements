78c4e09c14477f2b965bb408376738b555602014
Dinis Costa <scorpaust@hotmail.com>
2023-09-25 22:29:37 +0100

    Duration Gameplay Effects

--- Source/Aura/Public/Actor/AuraEffectActor.h
+++ Source/Aura/Public/Actor/AuraEffectActor.h
@@ -30,4 +30,7 @@ protected:
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Applied Effects");
 	TSubclassOf<UGameplayEffect> InstantGameplayEffectClass;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Applied Effects");
+	TSubclassOf<UGameplayEffect> DurationGameplayEffectClass;
 };
