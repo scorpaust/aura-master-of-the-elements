531535dcb8f25a887f8827e00db4ade4e865699b
Dinis <scorpaust@hotmail.com>
2023-12-16 01:14:12 +0000

    Native Gameplay Tags
    Primary and Secondary Attributes

---
+++ Source/Aura/Private/AuraGameplayTags.cpp
@@ -0,0 +1,58 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AuraGameplayTags.h"
+#include "GameplayTagsManager.h"
+
+FAuraGameplayTags FAuraGameplayTags::GameplayTags;
+
+void FAuraGameplayTags::InitializeNativeGameplayTags()
+{
+    /*
+    * Primary Attributes
+    */
+
+    GameplayTags.Attributes_Primary_Strength = UGameplayTagsManager::Get().AddNativeGameplayTag(
+        FName("Attributes.Primary.Strength"),
+        FString("Increases physical damage."));
+
+    GameplayTags.Attributes_Primary_Intelligence = UGameplayTagsManager::Get().AddNativeGameplayTag(
+        FName("Attributes.Primary.Intelligence"),
+        FString("Increases magical damage."));
+
+    GameplayTags.Attributes_Primary_Resilience = UGameplayTagsManager::Get().AddNativeGameplayTag(
+        FName("Attributes.Primary.Resilience"),
+        FString("Increases armor and armor penetration."));
+
+    GameplayTags.Attributes_Primary_Vigor = UGameplayTagsManager::Get().AddNativeGameplayTag(
+        FName("Attributes.Primary.Vigor"),
+        FString("Increases health."));
+
+    /*
+    * Secondary Attributes
+    */
+
+    GameplayTags.Attributes_Secondary_Armor = UGameplayTagsManager::Get().AddNativeGameplayTag(
+        FName("Attributes.Secondary.Armor"), 
+        FString("Reduces damage taken. Improves block chance"));
+
+    GameplayTags.Attributes_Secondary_ArmorPenetration = UGameplayTagsManager::Get().AddNativeGameplayTag(
+        FName("Attributes.Secondary.ArmorPenetration"),
+        FString("Ignored percentage of enemy armor. Increases critical hit chance."));
+
+    GameplayTags.Attributes_Secondary_BlockChance = UGameplayTagsManager::Get().AddNativeGameplayTag(
+        FName("Attributes.Secondary.BlockChance"),
+        FString("Chance to cut incoming damage in half."));
+
+    GameplayTags.Attributes_Secondary_CriticalHitChance = UGameplayTagsManager::Get().AddNativeGameplayTag(
+        FName("Attributes.Secondary.CriticalHitChance"),
+        FString("Chance to double damage plus critical hit bonus."));
+
+    GameplayTags.Attributes_Secondary_CriticalHitDamage = UGameplayTagsManager::Get().AddNativeGameplayTag(
+        FName("Attributes.Secondary.CriticalHitDamage"),
+        FString("Bonus damage added when a critical hit is scored."));
+
+    GameplayTags.Attributes_Secondary_CriticalHitResistance = UGameplayTagsManager::Get().AddNativeGameplayTag(
+        FName("Attributes.Secondary.CriticalHitResistance"),
+        FString("Reduces critical hit chance of attacking enemies."));
+}
